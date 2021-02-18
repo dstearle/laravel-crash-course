88728d0af1951d11f045f77e4bf240425b708597
dstearle <dallas.st.earle@gmail.com>
2021-02-18 10:30:29 -0800

    Installed bootstrap using this article as reference https://www.positronx.io/how-to-properly-install-and-use-bootstrap-in-laravel/

--- routes/web.php
+++ routes/web.php
@@ -53,3 +53,6 @@ Route::get('/about', [PagesController::class, 'about']);
 
 // Services Page
 Route::get('/services', [PagesController::class, 'services']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
