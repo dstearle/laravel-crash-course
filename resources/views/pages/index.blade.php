e0316649499db42b6fd055709314e3bb2df571fa
dstearle <dallas.st.earle@gmail.com>
2021-02-18 12:24:18 -0800

    Setup jumbotron with login and register buttons in the index

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,8 +2,25 @@
 
 @section('content')
 
-    <h1>{{ $title }}</h1>
+    {{-- Jumbotron --}}
+    <div class="jumbotron text-center">
 
-    <p>A basic Laravel application to brush up on the basics...</p>
+        {{-- Title --}}
+        <h1>{{ $title }}</h1>
+
+        {{-- Content --}}
+        <p>A basic Laravel application to brush up on the basics...</p>
+
+        <p>
+
+            {{-- Login --}}
+            <a href="/login" class="btn btn-success btn-lg">Login</a>
+
+            {{-- Register --}}
+            <a href="/register" class="btn btn-danger btn-lg">Register</a>
+
+        </p>
+
+    </div>
     
 @endsection
